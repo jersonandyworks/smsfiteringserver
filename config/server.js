fab62ab245efa47f458ccb172409445d298c7b6c
Jerson Andy Barrios <barriosjerson@gmail.com>
2021-02-21 22:01:53 +0800

    Initial commit for sms filtering system for wewe

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f4aacec6f9cfd5f7a6273111233b786b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b32a9bd1-8f67-4321-9daf-33b557897405'
+};
