1b3f6bb6704b8f6e768f89e041e6f49b0a9ce855
Jerson Andy Barrios <barriosjerson@gmail.com>
2021-03-23 16:02:07 +0800

    test

--- config/database.js
+++ config/database.js
@@ -24,9 +24,9 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
+        database: env('DATABASE_NAME', 'wewesmsfiltering'),
+        username: env('DATABASE_USERNAME', 'wewedbuser'),
+        password: env('DATABASE_PASSWORD', 'N0132Pass'),
       },
       options: {
         ssl: false,
